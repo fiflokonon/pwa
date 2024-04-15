b04c34bfc65693b0b83a62cb3d6b482cee3a8267
fiflokonon <fiflokonon@gmail.com>
2024-04-15 21:48:15 +0100

    Go away

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    // Ajoutez d'autres ressources à mettre en cache ici
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
